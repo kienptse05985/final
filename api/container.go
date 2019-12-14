51bac63a0aa283783258c4c0fcc959f9fcbc1b8b
HuyNV <huynvse05226@fpt.edu.vn>
2019-12-15 00:43:47 +0700

    remove mongo

--- api/container.go
+++ api/container.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/robfig/cron"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Container struct {
-	MongoClient *mongo.Client
 	Config      Config
 
 	CronDaemon     *cron.Cron
@@ -36,18 +32,6 @@ func (container *Container) InitContainer(config Config) (err error) {
 
 func (container *Container) LoadDependencies(config Config) (err error) {
 	container.Config = config
-	clientOptions := options.ClientOptions{
-		Auth: &options.Credential{
-			Username:   config.MongoUser,
-			Password:   config.MongoPassword,
-			AuthSource: config.MongoSource,
-		},
-	}
-	client, err := mongo.NewClient(clientOptions.ApplyURI(fmt.Sprintf("mongodb://%s", config.MongoServer)))
-	if err != nil {
-		return err
-	}
-	container.MongoClient = client
 	container.CronDaemon = cron.New()
 	container.MailRepository = &SendMailHandler{
 		Username: config.MailUserName,
